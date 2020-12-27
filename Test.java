ea80593021388936ffad474946e7af755329ae1d
bhanusoni72 <bhanusoni7777@gmail.com>
2020-12-27 13:55:34 +0530

    add runtime hungry status

--- Test.java
+++ Test.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
 class Test{
 
 	public static void main(String[]args){
-		System.out.println("i am hungry");
-
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter you are hungry or not in true or false");
+		boolean hungry=sc.nextBoolean();
+		if(hungry){
+			System.out.println("i am hungry");		
+			System.out.println("Eat Samosa");
+		}
+		else{
+			System.out.println("i am not hungry");
+			System.out.println("Do your home work");
+		}
 	}
 }
